c10dc159d41790ea6120b28f03948be040026b3f
Mohammed Taha <95439605+BMTmohammedtaha@users.noreply.github.com>
2023-05-13 15:21:22 +0200

    Create DateToString.php

---
+++ src/DateToString.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Effectra\ToString;
+
+class DateToString
+{
+    /**
+     * Formats a time value in seconds into HH:MM:SS format.
+     *
+     * @param int $time The time value in seconds.
+     * @return string The formatted time string (HH:MM:SS).
+     */
+    public function formatTime(int $time): string
+    {
+        $hours = gmdate('H', $time);
+        $minutes = gmdate('i', $time);
+        $seconds = gmdate('s', $time);
+
+        return sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds);
+    }
+
+    /**
+     * Formats a timestamp into YYYY-MM-DD format.
+     *
+     * @param int $timestamp The timestamp value.
+     * @return string The formatted date string (YYYY-MM-DD).
+     */
+    public function formatDate(int $timestamp): string
+    {
+        return date('Y-m-d', $timestamp);
+    }
+}
